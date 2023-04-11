bc8fb0641a597cd56228318139d45d3d7ef07a3f
Amanda <Amanda@example.com>
2023-04-10 17:51:21 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is similar to creating a multilingual puzzle");
+return (0);
+}
+
